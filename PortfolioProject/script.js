fc9384bbe62e9c4ecc3593e7425b4897c0410032
Mika <mika.tahti94@gmail.com>
2021-10-27 13:19:04 +0300

    Javascript for form added

---
+++ PortfolioProject/script.js
@@ -0,0 +1,24 @@
+/*Form interactivity */
+
+if (document.body.contains(document.getElementById('contactForm'))) {
+    const reset = document.getElementById('resetButton');
+    const submit = document.getElementById('submitButton');
+
+    const resetFunction = () => {
+        document.getElementById('contactForm').reset();
+    }
+
+    const submitFunction = () => {
+        alert('Your message would have been sent! Due to this page being entirely for practice however, the form does not really send the information anywhere.');
+    }
+
+    reset.onclick = resetFunction;
+    submit.onclick = submitFunction;
+}
+
+const emailValue = document.body.getElementById('email');
+const phoneValue = document.body.getElementById('phone');
+
+if (emailValue === null && phoneValue === null) {
+    alert('Please fill in at least your email or your phone number!')
+};
