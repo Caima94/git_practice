136906fd0c2f1e0886b74c117c131d8f5da4e2a1
Mika <mika.tahti94@gmail.com>
2021-10-27 14:07:42 +0300

    fix for phone/email validation

--- PortfolioProject/script.js
+++ PortfolioProject/script.js
@@ -3,19 +3,19 @@
 if (document.body.contains(document.getElementById('contactForm'))) {
     const resetForm = document.getElementById('resetButton');
     const submitForm = document.getElementById('submitButton');
-    const emailValue = document.getElementById('email');
-    const phoneValue = document.getElementById('phone');
 
     const resetFunction = () => {
         document.getElementById('contactForm').reset();
     }
 
     const submitFunction = () => {
-        if (emailValue === null && phoneValue === null) {
-            alert('Please fill in at least your email or your phone number!')
-        };
+        if (document.contactForm.email.value == "" && document.contactForm.phone.value == 0) {
+            alert('please provide at least your email or your phone number!');
+        }
         alert('Your message would have been sent! Due to this page being entirely for practice however, the form does not really send the information anywhere.');
     }
+
+    if (document.getElementById('email'))
 
     resetForm.onclick = resetFunction;
     submitForm.onclick = submitFunction;
