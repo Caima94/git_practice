ddf668f444b1cd3126b3ded86fed77034960bd61
Mika <mika.tahti94@gmail.com>
2021-10-27 19:12:23 +0300

    js fix

--- PortfolioProject/script.js
+++ PortfolioProject/script.js
@@ -3,14 +3,14 @@
 if (document.body.contains(document.getElementById('contactForm'))) {
     const resetForm = document.getElementById('resetButton');
     const submitForm = document.getElementById('submitButton');
-
+    
     const resetFunction = () => {
         document.getElementById('contactForm').reset();
     }
 
     const submitFunction = () => {
-        if ((document.contactForm.email.value == "") && (document.contactForm.phone.value == 0)) {
-            alert('please provide at least your email or your phone number!');
+        if ((document.getElementById('email').value.length == 0) && (document.getElementById('phone').value.length == 0)) {
+            alert('Please enter at least your email or your phone number!');
         }
         alert('Your message would have been sent! Due to this page being entirely for practice however, the form does not really send the information anywhere.');
     }
